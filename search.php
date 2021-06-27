1b30c118bd3f57d50e873bbd025e90385d90b4a1
Arshad Majgaonkar <arshadmaj608@gmail.com>
2021-06-27 13:07:55 +0530

    done : finished fullstack of both pages

--- search.php
+++ search.php
@@ -2,6 +2,49 @@
 
 require 'include/dbConfig.inc.php';
 
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>MyGoals | Search</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300&display=swap" rel="stylesheet">
+        
+	
+    </head>
+    <body>
+
+<!-- The fixed navbar will overlay your other content, unless you add padding to the bottom of the <body>. Tip: By default, the navbar is 50px high.  -->
+<nav class="navbar navbar-expand-lg  navbar-light bg-light">
+   
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+   <span class="navbar-toggler-icon"></span>
+   </button>
+   <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class="navbar-nav mr-auto">
+		  <h2>Goals Site</h2>
+         </div>
+      <form class="form-inline my-2 my-lg-0">
+         
+		  <div class="nav-item active">
+            <a class="nav-link text-dark" href="dash.php">Dashboard <span class="sr-only">(current)</span></a>
+         </div>
+		  
+		  <div class="nav-item">
+            <a class="nav-link text-dark" href="search.php">Search</a>
+         </div >
+		 <a class="nav-link text-dark" href="include/logout.inc.php">Logout</a>
+      </form>
+   </div>
+</nav>
+
+<?php
+
 if(isset($_POST['search-name']))
 {
     $searchName = mysqli_real_escape_string($con,$_POST['search-name']);
@@ -148,21 +191,12 @@ else
 
 ?>
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>MyGoals | Search</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300&display=swap" rel="stylesheet">
-        
-	
-    </head>
-    <body>
 
+
+
+
+
+?>
         <!-- Search form -->
         <form name="searchform" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
             <div class="row">
