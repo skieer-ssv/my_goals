41219be2f80b9405fe9b58d4f0f162f134dfd1db
skieer-ssv <55557885+skieer-ssv@users.noreply.github.com>
2021-06-27 10:07:34 +0530

    added: add goal layout and functionality with db

---
+++ include/add_goal.inc.php
@@ -0,0 +1,27 @@
+<?php
+if (isset($_POST['submit'])) {
+// TODO: add prepare statement 
+  session_start();
+  $title = strip_tags($_POST['NewsTitle']);
+  
+  $desc = $_POST['newsDescription'] ? strip_tags($_POST['newsDescription']) : NULL;
+  $alert = $_POST['alertDate'] ? strtotime(strip_tags($_POST['alertDate'])) : NULL;
+  
+  $uid= $_SESSION['uid'];
+  require_once 'dbquery.inc.php';
+  require_once 'dbConfig.inc.php';
+  $qy = "INSERT INTO `goals`(`goal`,`description`,`alert_date`,`uid`) VALUES( ?,?,?,?)";
+
+   try{
+    add_goal($con,$qy,"sssi",$title,$desc,$alert,$uid);
+   
+      echo '<script>alert("Successfully updated");document.location="../site-admin/add-news.php"</script>';
+   }
+   catch (PDOException $e) {
+    echo $e->getMessage();
+  }
+  
+  
+} else {
+  header("location: ../site-admin/add-news.php");
+}
